b45a6984908ed36049b70f8410633e40860aff7c
akasa <lyen.info@gmail.com>
2017-04-11 20:02:07 +0200

    ajout modif main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,27 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_instance" "www"{
+resource "aws_instance" "example"{
   ami = "ami-405f7226"
   instance_type = "t2.micro"
+  security_groups = ["$(aws_security_group.id)"]
+
+
 
   tags { 
     Name ="test-terraform"
   }
-	     
+
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }	     
 }
 
