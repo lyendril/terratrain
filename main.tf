e39be137532ebf410d09350ff15951d96664a73b
akasa <lyen.info@gmail.com>
2017-04-12 10:18:40 +0200

    ajout asg

--- main.tf
+++ main.tf
@@ -5,24 +5,22 @@ provider "aws" {
 resource "aws_instance" "example"{
   ami = "ami-405f7226"
   instance_type = "t2.micro"
-  security_groups = ["$(aws_security_group.id)"]
+  vpc_security_group_ids = ["${aws_security_group.example.id}"]  
 
-
+  user_data = "${file("userdata.sh")}"
 
   tags { 
     Name ="test-terraform"
   }
-
 }
 
-resource "aws_security_group" "instance" {
+resource "aws_security_group" "example" {
   name = "terraform-example-instance"
 
   ingress {
     from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
+    to_port   = 8080
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }	     
+    }  
 }
-
